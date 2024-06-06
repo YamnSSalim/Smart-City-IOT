0e25f05dc7c6f29bec3f7e6eec6bf09e184637d2
YamnSSalim <yamnssalim@gmail.com>
2024-06-07 00:31:10 +0200

    Project 2: Adding directions to the fuction. Edited the readJoyStickValue.

--- JoyStick/JoyStick.ino
+++ JoyStick/JoyStick.ino
@@ -8,13 +8,16 @@ const int yPin = 35;     //Joystick Y-axis pin
 
 int valueX = 0;          //X-axis analog storage variabel
 int valueY = 0;          //Y-axis analog storage variabel
+
+
 /*-------------------------*/
 
 
-void readJoyStickValue(int x, int y){  //Reads analog values from joystick 
+void readJoyStickValue(int &x, int &y){  //Reads analog values from joystick and adds changes to the parameters x and y 
     x = analogRead(xPin);
     y = analogRead(yPin);
 
+    
     Serial.print(" X-value = ");
     Serial.println(x);
 
@@ -22,6 +25,7 @@ void readJoyStickValue(int x, int y){  //Reads analog values from joystick
 
     Serial.print(" Y-Value = ");
     Serial.println(y);
+
 }
 
 
@@ -34,5 +38,19 @@ void setup(){
 
 void loop(){ 
     readJoyStickValue(valueX, valueY); //calling function in loop
+    int tol = 500;
+    if (valueX > 1810-tol && valueX < 1810+tol && valueY <= 0){
+        Serial.print("Direction: ");
+        Serial.println("UP");
+    }else if (valueX>=4090 && valueY > 1810-tol && valueY < 1810+tol){
+        Serial.print("Direction: ");
+        Serial.println("RIGHT");  
+    }else if (valueX > 1810-tol && valueX < 1810+tol && valueY >= 4090){
+        Serial.print("Direction: ");
+        Serial.println("DOWN");
+    }else if (valueX <=0 && valueY > 1810-tol && valueY <1810+tol){
+        Serial.print("Direction: ");
+        Serial.println("LEFT");
+    }
     delay(500);
 }
