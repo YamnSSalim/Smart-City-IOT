cd71a291d2d84a34afbb45e1d21cf6851a3a7b1f
YamnSSalim <yamnssalim@gmail.com>
2024-06-07 03:52:19 +0200

    Prototype 3: Connecting ESP32 to WiFi and publishing joystick movements to topic through MQTT protocol

--- JoyStick/JoyStick.ino
+++ JoyStick/JoyStick.ino
@@ -14,19 +14,49 @@ const char* pass = "Network1";  // WiFi
 // Function to connect ESP32 to WiFi
 void setupWiFi(){
 
-    Serial.printf("\nConnecting to %s\n\n", ssid);
+    Serial.printf("\nConnecting to %s\n\n", ssid);  //Print a message indicating the WiFi connection attempt
 
     WiFi.begin(ssid,pass);  // Initialize the connection to WiFi network
 
-    // Waiting for the ESP32 to connect to WiFI
+    // Loop until the ESP32 is connected to WiFi
     while (WiFi.status() != WL_CONNECTED){
         Serial.print(".");
         delay(100);
     }
     Serial.println("\nSuccessfully connected to the WiFi network");
     Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIP()); //Print the local IP address of the ESP32
 }
+
+
+///////////////////////
+//// PubSubClient /////
+///////////////////////
+
+/*------ PubSubClient-Variables ------*/
+const char* broker_server = "192.168.137.208";
+const char* outTopic = "joystick/data";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+/*------------------------------------*/
+
+void reconnect(){
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+
+        if (client.connect("ESP32ClientUser")){
+            Serial.println("connected");
+        }else{
+            Serial.print("failed, reconnect= ");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
 
 
 ////////////////////
@@ -48,6 +78,9 @@ void readJoyStickValue(int &x, int &y)
     x = analogRead(xPin);   // Read analog value from X-axis
     y = analogRead(yPin);   // Read analog value from Y-axis
 
+
+    //Debugging option
+    /* 
     Serial.print(" X-value = ");    // Print X-axis analog value
     Serial.println(x);
 
@@ -55,6 +88,7 @@ void readJoyStickValue(int &x, int &y)
 
     Serial.print(" Y-Value = ");    // Print Y-axis analog value
     Serial.println(y);
+    */
 }
 
 // Function to convert analog values into directions
@@ -65,11 +99,14 @@ void getDirection(int x, int y)
     const int centerValue = 1810;   // Center value 
     const int tol = 500;        // Tolerance value 
 
+    String direction;
+
     // Check if the joystick is pushed up
     if (x > centerValue - tol && x < centerValue + tol && y <= minValue)  
     {
         Serial.print("Direction: ");  
         Serial.println("UP");
+        direction = "UP";
     }
 
     // Check if the joystick is pushed right
@@ -77,6 +114,7 @@ void getDirection(int x, int y)
     {
         Serial.print("Direction: "); 
         Serial.println("RIGHT");
+        direction = "RIGHT";
     }
 
     // Check if the joystick is pushed down
@@ -84,6 +122,7 @@ void getDirection(int x, int y)
     {
         Serial.print("Direction: ");
         Serial.println("DOWN");
+        direction = "DOWN";
     }
 
     // Check if the joystick is pushed left
@@ -91,6 +130,12 @@ void getDirection(int x, int y)
     {
         Serial.print("Direction: ");
         Serial.println("LEFT");
+        direction = "LEFT";
+    }
+
+    // Publish direction to MQTT topic 
+    if (!direction.isEmpty()){
+        client.publish(outTopic, direction.c_str());
     }
 }
 
@@ -101,9 +146,22 @@ void setup()
     pinMode(xPin, INPUT);
     pinMode(yPin, INPUT);
     setupWiFi();
+    client.setServer(broker_server, 1883);
 }
 
 void loop()
 {
+    if(!client.connected()){
+        reconnect();
+    }
+    client.loop();
+
+    // Read joystick values
+    readJoyStickValue(valueX, valueY);
+
+    // Get joystick direction and publish to MQTT
+    getDirection(valueX, valueY);
+
+    delay(100);
 
 }
