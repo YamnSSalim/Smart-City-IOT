88e5e5cffc91b901bc90b4bbc7b882b7accb33ba
YamnSSalim <yamnssalim@gmail.com>
2024-06-07 05:24:06 +0200

    Prototype 4: Subscribing and serial printing data from onboard ESP32

--- ESP32onBoard/ESP32onBoard.ino
+++ ESP32onBoard/ESP32onBoard.ino
@@ -1,16 +1,18 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 ////////////////
 ///// WiFi /////
 ////////////////
 
 /*------ WiFi-Variables ------*/
-const char* ssid = "Studysession";
-const char* pass = "Network1";
+const char *ssid = "Studysession";
+const char *pass = "Network1";
 /*----------------------------*/
 
 // Function to connect ESP32 onboard to WiFi
-void setupWiFi(){
+void setupWiFi()
+{
     Serial.printf("\nConnecting to %s\n\n", ssid);
 
     WiFi.begin(ssid, pass);
@@ -23,13 +25,69 @@ void setupWiFi(){
     Serial.println("\nSuccessfully connected to the WiFi network");
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
-}   
-
-void setup(){
-    Serial.begin(115200);
-    setupWiFi();
 }
 
-void loop(){
+///////////////////////
+///// PubSubClient ////
+///////////////////////
 
+/*------ PubSubClient-Variables ------*/
+const char *broker_server = "192.168.137.208";
+const char *outTopic = "joystick/data";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+/*-----------------------------------*/
+
+void reconnect()
+{
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+
+        if (client.connect("ESP32ClientSubscriber"))
+        {
+            Serial.println("connected");
+            client.subscribe(outTopic);
+        }
+        else
+        {
+            Serial.print("failed, reconnect= ");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    setupWiFi();
+    client.setServer(broker_server, 1883);
+    client.setCallback(callback);
+}
+
+void loop()
+{
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
 }
