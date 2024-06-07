24c36c3c0f6f96ff0ecc769521617becd57a29e7
YamnSSalim <yamnssalim@gmail.com>
2024-06-07 20:36:30 +0200

    Prototype 5: Printing movement on Zumo32U4 OLED screen.

--- Zumo32U4/Zumo32U4.ino
+++ Zumo32U4/Zumo32U4.ino
@@ -1,9 +1,17 @@
 #include <Wire.h>
+#include <Zumo32U4.h>
 
+///////////////////////////////
+////// I2C-Communcation ///////
+///////////////////////////////
+
+/*------------ I2C-Variables ------------*/
 const int Zumo32U4Address = 0x08; // I2C address of the Zumo32U4
 volatile bool dataReceived = false; // Flag to indicate whether data has been received
 char receivedData[32]; // Stores received data
 int dataLength = 0; // Length of the received data 
+/*---------------------------------------*/
+
 
 // Function called when data is received via I2C
 void receiveEvent(int howMany)
@@ -19,10 +27,41 @@ void receiveEvent(int howMany)
     dataReceived = true; // Set the flag to indicate data has been received
 }
 
+
+
+/////////////////////
+/////// OLED ////////
+/////////////////////
+
+/*----- OLED - Variable -----*/
+Zumo32U4OLED oled;  // Stores library info in variable
+/*---------------------------*/
+
+// Function for initialise OLED-screen
+void setupOLED(){
+    oled.init();
+    oled.setLayout21x8();
+    oled.clear();
+}
+
+// Function to update the OLED display with a given message
+void updateOLEDDisplay(const char* label, const char* message){
+    oled.clear();
+
+    oled.gotoXY(2,0);
+    oled.print(label);
+
+    oled.gotoXY(2,2);
+    oled.print(message);
+}
+
+
+
+
 void setup()
 {
     Serial.begin(115200);
-
+    setupOLED();
     Wire.begin(Zumo32U4Address);    // Initialize I2C communication
     Wire.onReceive(receiveEvent);   // Register the receive event handler
 }
@@ -34,6 +73,10 @@ void loop()
     {
         Serial.print("Received data: ");
         Serial.println(receivedData);   // Print the received data to the serial monitor 
+
+        // Update the OLED display with the received
+        updateOLEDDisplay("Received:", receivedData);
+
         dataReceived = false;   // Reset the flag
     }
 }
